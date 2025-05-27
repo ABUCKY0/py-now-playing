2ba970517ca6719a02cbdfe3a546f9b9cc7c96b4
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2025-05-26 20:16:45 -0500

    docs: :memo: Add module docstring to __init__.py to stop pylint from complaining

--- py_now_playing/__init__.py
+++ py_now_playing/__init__.py
@@ -1,4 +1,8 @@
-#is package
+"""
+py_now_playing - A Python library for interacting with the Now Playing API.
+This library provides classes and functions to retrieve and manage information about the currently playing media,
+including playback status, media timeline, and media details.
+"""
 from py_now_playing.media_timeline import MediaTimeline
 from py_now_playing.playback_info import PlaybackInfo, MediaPlaybackStatus, MediaPlaybackType, MediaPlaybackAutoRepeatMode
 from py_now_playing.pnp import PlaybackControls
